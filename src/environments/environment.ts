950a8759b22265029ce5d4f1900b561f7a941b62
ashraful.alam <ashrafulrasel21@gmail.com>
2021-11-17 23:06:48 +0600

    adding featuers and rating

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   weeklyMenuPlanning: 'http://localhost:9022/'
 };
 
